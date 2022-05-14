7c8afeda63dfa364a1ec1b8e6f30f2a5b8232f50
edvinas-kaziunas <edvinas.kaziunas@stud.vilniustech.lt>
2022-05-14 22:23:33 +0300

    Finnished player class x movement, added drawing of backroun and player

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -12,32 +12,35 @@ class car
 {
 private:
 	Player player;
+	double px = WINDOW_WIDTH / 2, py = 100;
 	const float dx = 7.0f;
-	player.x = WINDOW_WIDTH / 2;
-	player.y = 100;
 
 public:
 	void operator ++()
 	{
-		player.x += dx;
+		px += dx;
 	}
 	void operator --()
 	{
-		player.x -= dx;
+		px -= dx;
 	}
 	double get_x()
 	{
-		return player.x;
+		return px;
+	}
+	double get_y()
+	{
+		return py;
 	}
 
 };
 
 int main()
 {
-	double positionx, positiony;
+	//double positionx, positiony;
 	car s;
 	srand((unsigned)time(nullptr));
-	RenderWindow app(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Long Road 2");
+	RenderWindow app(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Long Road Deluxe");
 	app.setFramerateLimit(60);
 
 	Texture tBackground, tPlayer, tPlatform, tPowerup1, tPowerup5;
@@ -56,14 +59,33 @@ int main()
 	Sprite sprPowerup100(tPowerup1);
 	Sprite sprPowerup500(tPowerup5);
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	while (app.isOpen())
 	{
-		--s;
-		positionx=get_x();
+		Event e;
+		while (app.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				app.close();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			--s;
+			//positionx = s.get_x();
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			++s;
+			//positionx = s.get_x();
+		}
+
+		app.draw(sprBackground);
+
+		sprPlayer.setPosition(s.get_x(), s.get_y());
+		app.draw(sprPlayer);
+
+		app.display();
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-	{
-		++s;
-		positiony=s.get_x();
-	}
+	
+	return 0;
 }
