f1d8c08904098e839c3c08de3a6353479f23d9a1
edvinas-kaziunas <edvinas.kaziunas@stud.vilniustech.lt>
2022-05-16 19:05:39 +0300

    added thow and catch

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Audio.hpp>
 #include <format>
 #include <vector>
+#include <map>
 
 #include "Objects.h"
 #include "Utils.h"
@@ -101,6 +102,12 @@ public:
 	{
 		ey -= 6;
 	}
+	float Minus(float s)
+	{
+		float sm;
+		if (s < 100) throw 99;
+		else return s - 100;
+	}
 
 };
 int main()
@@ -110,6 +117,9 @@ int main()
 	Enemy2 en2[PLATES_AMOUNT];
 	Powerup pu;
 	Powerdown pd;
+	map<int, int> p;
+	p[1];
+	p[2];
 
 	srand((unsigned)time(nullptr));
 	RenderWindow app(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Long Road Deluxe");
@@ -137,6 +147,33 @@ int main()
 	text.setOutlineThickness(3);
 	text.setOutlineColor(Color::Black);
 	text.setPosition(WINDOW_WIDTH / 2.0f - 25.f, WINDOW_HEIGHT - 100.f);
+
+	sf::Text text2;
+	text2.setFont(font);
+	text2.setString("0");
+	text2.setCharacterSize(60);
+	text2.setFillColor(Color::Magenta);
+	text2.setOutlineThickness(5);
+	text2.setOutlineColor(Color::Black);
+	text2.setPosition(WINDOW_WIDTH / 2.0f - 30.f, WINDOW_HEIGHT - 600.f);
+
+	sf::Text text3;
+	text3.setFont(font);
+	text3.setString("0");
+	text3.setCharacterSize(70);
+	text3.setFillColor(Color::Red);
+	text3.setOutlineThickness(7);
+	text3.setOutlineColor(Color::Black);
+	text3.setPosition(WINDOW_WIDTH / 2.0f - 170.f, WINDOW_HEIGHT / 2);
+
+	sf::Text text4;
+	text4.setFont(font);
+	text4.setString("Powerups collected: ");
+	text4.setCharacterSize(20);
+	text4.setFillColor(Color::Blue);
+	text4.setOutlineThickness(2);
+	text4.setOutlineColor(Color::Black);
+	text4.setPosition(WINDOW_WIDTH / 2.0f - 250.f, WINDOW_HEIGHT-450.f);
 
 	Sprite sprBackground(tBackground);
 	Sprite sprPlayer(tPlayer);
@@ -168,47 +205,75 @@ int main()
 		}
 
 		app.draw(sprBackground);
-
-		for (int i = 0; i < PLATES_AMOUNT; ++i)
+		if(fail==0)
 		{
-			Movement* move1 = &en1[i];
-			move1->MoveDown();
-			if (move1->GetY() < 0)
-				move1->cmore();
-			sprEnemy.setPosition(en1[i].GetX(), en1[i].GetY());
-			app.draw(sprEnemy);
+			for (int i = 0; i < PLATES_AMOUNT; ++i)
+			{
+				Movement* move1 = &en1[i];
+				move1->MoveDown();
+				if (move1->GetY() < 0)
+					move1->cmore();
+				sprEnemy.setPosition(en1[i].GetX(), en1[i].GetY());
+				app.draw(sprEnemy);
+			}
+
+			for (int i = 0; i < PLATES_AMOUNT; ++i)
+			{
+				Movement* move2 = &en2[i];
+				move2->MoveDown();
+				if (move2->GetY() < 0)
+					move2->cmore();
+				sprEnemy2.setPosition(en2[i].GetX(), en2[i].GetY());
+				app.draw(sprEnemy2);
+			}
+
+			Movement* move3 = &pu;
+			move3->MoveDown();
+			if (move3->GetY() < 0)
+				move3->cmore();
+			sprPowerup100.setPosition(pu.GetX(), pu.GetY());
+			app.draw(sprPowerup100);
+
+			Movement* move4 = &pd;
+			move4->MoveDown();
+			if (move4->GetY() < 0)
+				move4->cmore();
+			sprPowerdown.setPosition(pd.GetX(), pd.GetY());
+			app.draw(sprPowerdown);
+
+			score += 0.1;
+		}
+		else
+		{
+			if (score > high) high = score;
+			score = 0;
+			text3.setString("Game Over");
+			app.draw(text3);
+			app.draw(text4);
 		}
 
-		for (int i = 0; i < PLATES_AMOUNT; ++i)
-		{
-			Movement* move2 = &en2[i];
-			move2->MoveDown();
-			if (move2->GetY() < 0)
-				move2->cmore();
-			sprEnemy2.setPosition(en2[i].GetX(), en2[i].GetY());
-			app.draw(sprEnemy2);
-		}
-
-		Movement* move3 = &pu;
-		move3->MoveDown();
-		if (move3->GetY() < 0)
-			move3->cmore();
-		sprPowerup100.setPosition(pu.GetX(), pu.GetY());
-		app.draw(sprPowerup100);
-
-		Movement* move4 = &pd;
-		move4->MoveDown();
-		if (move4->GetY() < 0)
-			move4->cmore();
-		sprPowerdown.setPosition(pd.GetX(), pd.GetY());
-		app.draw(sprPowerdown);
-
-		score += 0.1;
+		//catch (float n) // jeigu prie throw parasomas skaicius
+		//{
+			//score = 0;
+		//}
+		
 		text.setString(std::format("{:.0f}", score));
 		app.draw(text);
 
+		text2.setString(std::format("{:.0f}", high));
+		app.draw(text2);
+
 		sprPlayer.setPosition(s.GetX(), s.GetY());
 		app.draw(sprPlayer);
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::T))
+		{
+			fail++;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+		{
+			fail = 0;
+		}
 
 		app.display();
 	}
